1ebd6eca75e7d3028c4580bbec10e5e15834379e
LordReaper <39646826+LordReaperY@users.noreply.github.com>
2020-09-17 15:16:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "prefix": "c!",
+    "token_bot": "NzQzNDc5MjM0MjEwNTYyMDU5.XzVQ_g.6gMMzz7W273hms38Z56hVhrFFno",
+    "youtube_api": "AIzaSyBNTVZo1sk54hW6OURSrO-ppUijx9sTvKQ",
+    "colors": {
+      "error": "0xFF0000",
+      "success": "0x00FF46",
+      "warning": "0xFFD100"
+    },
+    "emotes": {
+    "music": "🎶 ",
+    "queue": "🎞️",
+    "error": "❌",
+    "success": "☑️",
+    "warning": "⚠️",
+    "repeat": "🔁",
+    "pause": "⏸️",
+    "resume": "⏯️",
+    "stop": "⏹️"
+    }
+}
